1acef2a3600e4e4411dadae06593fc73e63f908c
mrkost <mr.kostman@gmail.com>
2025-05-10 04:25:20 +0300

    update mera

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:bundle_resources_loader/bundle_resources_loader.dart';
 
 void main() {
